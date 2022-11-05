79043076eef212d67ddc6f156532fb9f378b4139
Ahmet Nazmi BOZHÜYÜK <67506232+ahmetnazmibozhuyuk@users.noreply.github.com>
2022-11-05 17:15:39 +0300

    basic controls added

--- Assets/_Scripts/Control/PlayerControl.cs
+++ Assets/_Scripts/Control/PlayerControl.cs
@@ -6,16 +6,22 @@ namespace Pounds.Control
     public class PlayerControl : MonoBehaviour
     {
         #region Inputs
-        private bool _leftInput { get { return Input.GetKey(KeyCode.LeftArrow); } }
-        private bool _rightInput { get { return Input.GetKey(KeyCode.RightArrow); } }
+        private bool _leftInput { get { return Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A); } }
+        private bool _leftInputReleased { get { return Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A); } }
+        private bool _rightInput { get { return Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D); ; } }
+        private bool _rightInputReleased { get { return Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKey(KeyCode.D); ; } }
         private bool _jumpInput { get { return Input.GetKeyDown(KeyCode.Space); } }
+        private bool _jumpInputReleased { get { return Input.GetKeyUp(KeyCode.Space); } }
         #endregion
 
 
-        [SerializeField] private float movementSpeed;
-        [SerializeField] private float jumpPower;
+        [SerializeField] private float movementSpeed = 20f;
+        [SerializeField] private float jumpPower = 120f;
 
         private bool _isMovingLeft;
+        private bool _isInAir;
+
+        private Vector3 _currentVelocityVector;
 
         private Rigidbody _rigidbody;
 
@@ -26,24 +32,29 @@ namespace Pounds.Control
 
         private void Update()
         {
+
+            if (CheckIfPlayerOnLand())
+            {
+                Debug.Log("on land");
+            }
+
             MovementAction();
             JumpAction();
 
 
 
         }
-
         private void MovementAction()
         {
             if (_leftInput)
             {
-                _rigidbody.AddForce(-30, 0, 0);
+                SetVelocityX(-movementSpeed);
                 Debug.Log("left input");
                 return;
             }
             if (_rightInput)
             {
-                _rigidbody.AddForce(30, 0, 0);
+                SetVelocityX(movementSpeed);
                 Debug.Log("right input");
                 return;
             }
@@ -52,9 +63,25 @@ namespace Pounds.Control
         {
             if (_jumpInput)
             {
-                _rigidbody.AddForce(Vector3.up * jumpPower);
+                SetVelocityY(jumpPower);
                 Debug.Log("jump input");
             }
+        }
+
+        private void SetVelocityX(float xVelocity)
+        {
+            _currentVelocityVector.Set(xVelocity, _rigidbody.velocity.y, _rigidbody.velocity.z);
+            _rigidbody.velocity = _currentVelocityVector;
+        }
+        private void SetVelocityY(float yVelocity)
+        {
+            _currentVelocityVector.Set(_rigidbody.velocity.x, yVelocity, _rigidbody.velocity.z);
+            _rigidbody.velocity = _currentVelocityVector;
+        }
+        private bool CheckIfPlayerOnLand()
+        {
+            //return Physics.BoxCast(transform.position, transform.position, Vector3.up);
+            return Physics.Raycast(transform.position, Vector3.up, -1);
         }
     }
 }
